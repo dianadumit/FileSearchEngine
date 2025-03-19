0ccc688dda4d16f85e1477d5acb5ebe0227e381f
dianadumit <dumitrescuddiana@yahoo.com>
2025-03-19 13:39:45 +0200

    feat(files): add recursive file finder with text reading
    The findFiles class recursively searches directories and reads text files line by line. It also tracks the total number of files and directories scanned.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FindFiles findFiles = new FindFiles();
+        String pathToFiles = "C://Users//dumit//Desktop//myFiles";
+        File file = new File(pathToFiles);
+        findFiles.findFiles(file);
+        System.out.println(findFiles);
     }
 }
