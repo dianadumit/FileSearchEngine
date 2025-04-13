edbd06b50474b61f55d0e3cc579cc18f37c84153
dianadumit <dumitrescuddiana@yahoo.com>
2025-04-13 21:36:47 +0300

    feat(master): add caching for search results

--- FileSearchEngineWorkers/src/main/java/org/example/searchmaster/CommunicationService.java
+++ FileSearchEngineWorkers/src/main/java/org/example/searchmaster/CommunicationService.java
@@ -1,6 +1,9 @@
 package org.example.searchmaster;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.example.searchworker.*;
@@ -10,11 +13,15 @@ import java.util.List;
 
 @Service
 public class CommunicationService {
+    private static final Logger logger = LoggerFactory.getLogger(CommunicationService.class);
     @Value("${worker.urls}")
     private String workerUrls;
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Cacheable("searchResults")
     public List<String> sendSearchRequest(String query){
+
+        logger.info("Computing result for query: {}. ", query);
         List<String> results = new ArrayList<>();
         String[] urls = workerUrls.split(",");
         for (String url : urls) {
--- FileSearchEngineWorkers/src/main/java/org/example/searchmaster/MasterController.java
+++ FileSearchEngineWorkers/src/main/java/org/example/searchmaster/MasterController.java
@@ -1,6 +1,10 @@
 package org.example.searchmaster;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,11 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MasterController {
+    private static final Logger logger = LoggerFactory.getLogger(MasterController.class);
     @Autowired
     private CommunicationService communicationService;
+    @Autowired
+    private CacheManager cacheManager;
+
     @PostMapping("/search")
-    public List<String> executeSearch(@RequestBody MasterSearchRequest request){
+    public List<String> executeSearch(@RequestBody MasterSearchRequest request) {
         String query = request.getQuery();
+        Cache cache = cacheManager.getCache("searchResults");
+
+        if (cache != null && cache.get(query) != null) {
+            logger.info("Result for query '{}' retrieved from cache", query);
+        } else {
+            logger.info("Result for query '{}' not found in cache", query);
+        }
         List<String> results = communicationService.sendSearchRequest(query);
         //add sorting for results
         return results;
